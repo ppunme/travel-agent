745fb847fcee66fe005f27f047a4db736c3f3fd8
Apikritch Rattanapisankul <apikritch.r@gmail.com>
2023-05-18 02:10:29 +1000

    added fontawesome

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,21 +1,54 @@
 <template>
-  <nav class="h-28 grid grid-cols-3 content-center">
-    <div class="flex justify-start">Logo</div>
-    <div class="flex gap-28 justify-center">
-      <router-link to="/">หน้าแรก</router-link>
-      <router-link to="/tours">แพ็คเกจทัวร์</router-link>
-      <router-link to="/contact">ติดต่อเรา</router-link>
+  <nav class="h-36 grid grid-cols-3 content-center">
+    <div class="flex justify-start pl-24">
+      <img src="@/assets/img/logo.png" alt="" />
+      <h4 class="ml-4 self-center font-semibold w-40 logo-text">
+        Wellness Life Travel
+      </h4>
     </div>
-    <div class="flex justify-end">Socail Media</div>
+    <div class="flex gap-28 justify-center">
+      <router-link class="self-center" to="/">หน้าแรก</router-link>
+      <router-link class="self-center" to="/tours">แพ็คเกจทัวร์</router-link>
+      <router-link class="self-center" to="/contact">ติดต่อเรา</router-link>
+    </div>
+    <div class="flex justify-end">
+      <div
+        class="social-media-bg w-60 h-20 pl-6 pr-2 self-center rounded-l-[5rem] flex justify-around"
+      >
+        <font-awesome-icon
+          :icon="['fab', 'square-facebook']"
+          size="2xl"
+          style="color: #ffffff"
+          class="self-center"
+        />
+        <font-awesome-icon
+          :icon="['fab', 'instagram']"
+          size="2xl"
+          style="color: #ffffff"
+          class="self-center"
+        />
+        <font-awesome-icon
+          :icon="['fab', 'twitter']"
+          size="2xl"
+          style="color: #ffffff"
+          class="self-center"
+        />
+      </div>
+    </div>
   </nav>
 </template>
 
 <script setup></script>
 
 <style lang="scss" scoped>
-@import "@/assets/scss/main.scss";
+@import "@/assets/scss/variables.scss";
 
-a {
+a,
+.logo-text {
   color: $primary-color;
+}
+
+.social-media-bg {
+  background-color: $secondary-color;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,25 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index.js";
 import PrimeVue from "primevue/config";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 import "./assets/scss/main.scss";
 import "primevue/resources/themes/lara-light-indigo/theme.css";
 import "primevue/resources/primevue.min.css";
 
+import {
+  faSquareFacebook,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
+library.add(faSquareFacebook, faInstagram, faTwitter);
+
 const app = createApp(App);
 
-app.use(router).use(PrimeVue).mount("#app");
+app
+  .use(router)
+  .use(PrimeVue)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
