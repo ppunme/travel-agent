b30873f02cfe97d1d98e9d59d42e77bca62ba4c3
Apikritch Rattanapisankul <apikritch.r@gmail.com>
2023-05-18 14:57:03 +1000

    move toolbar to child component

--- src/views/App/Tours.vue
+++ src/views/App/Tours.vue
@@ -1,65 +1,16 @@
 <template>
-  <div class="tool-bar py-12 text-center">
-    <h2 class="mb-8">แพ็คเกจทัวร์</h2>
-    <div class="grid grid-cols-3">
-      <div class="justify-self-end">
-        <Dropdown
-          v-model="selectedCountry"
-          :options="countries"
-          optionLabel="name"
-          placeholder="ประเทศ"
-          class="country-dropdown min-w-[225px] text-start pl-2"
-        >
-          <template #value="slotProps">
-            <div v-if="slotProps.value" class="flex align-items-center">
-              <div>{{ slotProps.value.name }}</div>
-            </div>
-            <span v-else>
-              {{ slotProps.placeholder }}
-            </span>
-          </template>
-          <template #option="slotProps">
-            <div class="flex align-items-center">
-              <div>{{ slotProps.option.name }}</div>
-            </div>
-          </template>
-        </Dropdown>
-      </div>
-      <div>
-        <span class="p-input-icon-right w-3/4 search">
-          <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
-          <InputText v-model="value1" placeholder="ค้นหา..." class="w-full" />
-        </span>
-      </div>
-      <div class="justify-self-start">
-        <Dropdown
-          v-model="selectedSort"
-          :options="sort"
-          optionLabel="name"
-          placeholder="เรียงตาม"
-          class="sort-dropdown min-w-[225px] text-start pl-2"
-        >
-          <template #value="slotProps">
-            <div v-if="slotProps.value" class="flex align-items-center">
-              <div>{{ slotProps.value.name }}</div>
-            </div>
-            <span v-else>
-              {{ slotProps.placeholder }}
-            </span>
-          </template>
-          <template #option="slotProps">
-            <div class="flex align-items-center">
-              <div>{{ slotProps.option.name }}</div>
-            </div>
-          </template>
-        </Dropdown>
-      </div>
-    </div>
-  </div>
+  <Tours
+    :selectedCountry="selectedCountry"
+    :countries="countries"
+    :selectedSort="selectedSort"
+    :sort="sort"
+    :value1="value1"
+  />>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import Tours from "@/components/Toolbar.vue";
 
 const selectedCountry = ref();
 const countries = ref([
@@ -84,52 +35,4 @@ const value1 = ref(null);
 // });
 </script>
 
-<style lang="scss" scoped>
-@import "@/assets/scss/variables.scss";
-
-.tool-bar {
-  background-color: #dfe9f0;
-}
-
-.p-dropdown {
-  border: 1px solid $secondary-font-color;
-  border-radius: 10rem;
-
-  &:not(.p-disabled) {
-    &:hover {
-      border-color: $secondary-color;
-    }
-    &.p-focus {
-      box-shadow: 0 0 0 0.2rem rgb(191 219 254);
-      border-color: $secondary-color;
-    }
-  }
-}
-
-.p-input-icon-right {
-  .p-inputtext {
-    border: 1px solid $secondary-font-color;
-    border-radius: 10rem;
-    padding-left: 1.25rem;
-
-    &::placeholder {
-      color: $secondary-font-color;
-    }
-
-    &:enabled {
-      &:hover {
-        border-color: $secondary-color;
-      }
-
-      &:focus {
-        box-shadow: 0 0 0 0.2rem rgb(191 219 254);
-        border-color: $secondary-color;
-      }
-    }
-  }
-
-  svg:last-of-type {
-    right: 1.25rem;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
