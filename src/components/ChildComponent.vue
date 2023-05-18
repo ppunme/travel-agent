e26198ecce03d032db3e83cd16cae4568c594a02
ppoonnamee <ppoonnamee@gmail.com>
2023-05-18 12:55:51 +0700

    Example passing props to children comp and use v-model

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <input />
+  <div>
+    <h2>Title</h2>
+    <input type="text" :value="title" @input="updateTitle" />
+    <h2>Content</h2>
+    <input type="text" :value="modelValue" @input="updateContent" />
+  </div>
+</template>
+
+<script setup>
+import { defineEmits, defineProps } from "vue";
+
+const props = defineProps({
+  title: String,
+  modelValue: String,
+});
+
+console.log("props", props);
+
+const emit = defineEmits(["update:title", "update:modelValue"]);
+
+const updateTitle = (event) => {
+  emit("update:title", event.target.value);
+};
+
+const updateContent = (event) => {
+  emit("update:modelValue", event.target.value);
+};
+</script>
---
+++ src/components/Parent.vue
@@ -0,0 +1,13 @@
+<template>
+  <ChildComponent v-model="content" v-model:title="postTitle" />
+  <p>content:: {{ content }}</p>
+  <p>post title::{{ postTitle }}</p>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import ChildComponent from "./ChildComponent.vue";
+
+const content = ref("");
+const postTitle = ref("test");
+</script>
