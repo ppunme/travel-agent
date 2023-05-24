e00b6f02ee32a0c647220a81fcf539564027e59f
Apikritch Rattanapisankul <apikritch.r@gmail.com>
2023-05-25 03:44:51 +1000

    added validation for text input

--- src/main.js
+++ src/main.js
@@ -119,7 +119,7 @@ import {
 
 import {
   faMagnifyingGlass,
-  faCircleXmark as solidXmark,
+  faCircleXmark as solidCircleXmark,
   faLocationDot,
   faClock,
   faPlane,
@@ -131,9 +131,11 @@ import {
   faTrash,
   faChevronUp,
   faChevronDown,
+  faCheck,
+  faXmark,
 } from "@fortawesome/free-solid-svg-icons";
 
-import { faCircleXmark as regularXmark } from "@fortawesome/free-regular-svg-icons";
+import { faCircleXmark as regularCircleXmark } from "@fortawesome/free-regular-svg-icons";
 
 library.add(
   faFacebook,
@@ -146,7 +148,7 @@ library.add(
   faTwitter,
   faMagnifyingGlass,
   faFacebookMessenger,
-  solidXmark,
+  solidCircleXmark,
   faLocationDot,
   faClock,
   faPlane,
@@ -155,7 +157,9 @@ library.add(
   faTrash,
   faChevronUp,
   faChevronDown,
-  regularXmark,
+  regularCircleXmark,
+  faCheck,
+  faXmark
 );
 
 const app = createApp(App);
--- src/views/App/TourManagement.vue
+++ src/views/App/TourManagement.vue
@@ -1,118 +1,148 @@
 <template>
   <div class="container mx-auto py-12">
-    <h2 class="text-center mb-8">เพิ่มแพ็คเกจใหม่</h2>
+    <h2 class="text-center mb-8">
+      {{
+        route.path.includes("create")
+          ? "เพิ่มแพ็คเกจใหม่"
+          : route.path.includes("edit") && "แก้ไขแพ็คเกจ"
+      }}
+    </h2>
     <h5 class="font-medium mb-6">Preview</h5>
     <div class="grid grid-cols-3 gap-40">
       <div>
         <TourPackageCard :item="tour" :management="true" />
       </div>
       <div class="col-span-2">
-        <div class="grid grid-cols-8 gap-8 mb-12 items-center">
-          <div class="col-start-1">รูปภาพ</div>
+        <form @submit="onSubmit">
+          <div class="grid grid-cols-8 gap-8 mb-12 items-center">
+            <div class="col-start-1">รูปภาพ</div>
 
-          <div class="col-span-7 flex">
-            <Chip v-if="clearButton" :label="tour.fileName" class="mr-2" />
+            <div class="col-span-7 flex">
+              <Chip v-if="clearButton" :label="tour.fileName" class="mr-2" />
 
-            <FileUpload
-              ref="fileUpload"
-              class="upload-package-image-button p-button-rounded !bg-primary-blue"
-              :class="tour.fileName && '!hidden'"
-              mode="basic"
-              name="image[]"
-              accept="image/*"
-              :maxFileSize="1000000"
-              @select="onSelectedFiles"
-            />
-            <button v-if="clearButton" @click="clearFile">
-              <font-awesome-icon
-                :icon="['far', 'circle-xmark']"
-                size="xl"
-                style="color: #d42e35"
+              <FileUpload
+                ref="fileUpload"
+                class="upload-package-image-button p-button-rounded !bg-primary-blue"
+                :class="tour.fileName && '!hidden'"
+                mode="basic"
+                name="image[]"
+                accept="image/*"
+                :maxFileSize="1000000"
+                @select="onSelectedFiles"
               />
-            </button>
+
+              <button v-if="clearButton" @click="clearFile">
+                <font-awesome-icon
+                  :icon="['far', 'circle-xmark']"
+                  size="xl"
+                  style="color: #d42e35"
+                />
+              </button>
+            </div>
+
+            <div class="col-start-1">หัวข้อ</div>
+            <div class="col-span-7">
+              <MyInputText
+                v-model:name="name"
+                placeholder="หัวข้อ"
+                v-model:error="nameError"
+              />
+            </div>
+
+            <div class="col-start-1">ประเทศ</div>
+            <MultiSelect
+              v-model="tour.countries"
+              :options="countries"
+              optionLabel="name"
+              placeholder="เลือกประเทศ"
+              display="chip"
+              class="col-start-2 col-span-7 pl-2 !rounded-full"
+              filter
+            >
+              <template #option="slotProps">
+                <div class="flex align-items-center">
+                  <div>{{ slotProps.option.name }}</div>
+                </div>
+              </template>
+              <template #footer>
+                <div class="py-2 px-3">
+                  <b>{{ tour.countries ? tour.countries.length : 0 }}</b>
+                  ประเทศ
+                </div>
+              </template>
+            </MultiSelect>
+
+            <div class="col-start-1">ระยะเวลา</div>
+            <InputText
+              v-model="tour.days"
+              placeholder="5"
+              class="all-input text-center !rounded-full"
+            />
+            <div>วัน</div>
+            <InputText
+              v-model="tour.nights"
+              placeholder="5"
+              class="all-input text-center !rounded-full"
+            />
+            <div>คืน</div>
+
+            <div class="col-start-1">ราคา</div>
+            <InputText
+              v-model="tour.price"
+              placeholder="10000"
+              class="all-input col-span-2 !rounded-full"
+            />
+            <div>บาท</div>
+
+            <div class="col-start-1">สายการบิน</div>
+            <div class="col-span-7">
+              <MyInputText
+                v-model:name="airline"
+                placeholder="สายการบิน"
+                v-model:error="airlineError"
+              />
+            </div>
           </div>
 
-          <div class="col-start-1">หัวข้อ</div>
-          <InputText
-            v-model="tour.name"
-            placeholder="หัวข้อ"
-            class="all-input col-span-7 !rounded-full"
-          />
-
-          <div class="col-start-1">ประเทศ</div>
-
-          <MultiSelect
-            v-model="tour.countries"
-            :options="countries"
-            optionLabel="name"
-            placeholder="เลือกประเทศ"
-            display="chip"
-            class="col-start-2 col-span-7 pl-2 !rounded-full"
-            filter
-          >
-            <template #option="slotProps">
-              <div class="flex align-items-center">
-                <div>{{ slotProps.option.name }}</div>
-              </div>
-            </template>
-            <template #footer>
-              <div class="py-2 px-3">
-                <b>{{ tour.countries ? tour.countries.length : 0 }}</b>
-                ประเทศ
-              </div>
-            </template>
-          </MultiSelect>
-
-          <div class="col-start-1">ระยะเวลา</div>
-          <InputText
-            v-model="tour.days"
-            placeholder="5"
-            class="all-input text-center !rounded-full"
-          />
-          <div>วัน</div>
-          <InputText
-            v-model="tour.nights"
-            placeholder="5"
-            class="all-input text-center !rounded-full"
-          />
-          <div>คืน</div>
-
-          <div class="col-start-1">ราคา</div>
-          <InputText
-            v-model="tour.price"
-            placeholder="10000"
-            class="all-input col-span-2 !rounded-full"
-          />
-          <div>บาท</div>
-
-          <div class="col-start-1">สายการบิน</div>
-          <InputText
-            v-model="tour.airline"
-            placeholder="สายการบิน"
-            class="all-input col-span-7 !rounded-full"
-          />
-        </div>
-        <div class="mb-4">รายละเอียดการเดินทาง</div>
-        <Editor v-model="tour.details" editorStyle="height: 320px" />
-        <div class="flex justify-end">
-          <Button class="w-48 !bg-[#06C755] !mt-12" rounded>
-            <font-awesome-icon :icon="['fas', 'plus']" size="2xl" /><span
-              class="mx-auto"
-              >เพิ่มแพ็คเกจใหม่</span
+          <div class="mb-4">รายละเอียดการเดินทาง</div>
+          <Editor v-model="tour.details" editorStyle="height: 320px" />
+          <div class="flex justify-end pt-12">
+            <Button
+              class="w-36 !bg-[#FFFFFF] !text-[#D42E35] !border-[#D42E35] !mr-6"
+              rounded
             >
-          </Button>
-        </div>
+              <font-awesome-icon :icon="['fas', 'xmark']" size="xl" /><span
+                class="mx-auto"
+                >ยกเลิก</span
+              >
+            </Button>
+            <Button
+              type="submit"
+              class="w-36 !bg-[#06C755] !border-[#06C755]"
+              rounded
+            >
+              <font-awesome-icon :icon="['fas', 'check']" size="xl" /><span
+                class="mx-auto"
+                >บันทึก</span
+              >
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   </div>
 </template>
 <script setup>
-import { ref, watchEffect, watch, onMounted } from "vue";
+import { watch, ref, watchEffect, onMounted } from "vue";
 import { useRoute } from "vue-router";
+import { useField, useForm } from "vee-validate";
+
 import TourPackageCard from "@/components/TourPackageCard.vue";
 import { data } from "@/services/TourPackageService";
+import MyInputText from "@/components/MyInputText.vue";
 
 const route = useRoute();
+const { handleSubmit } = useForm();
 
 const fileUpload = ref(null);
 const clearButton = ref(false);
@@ -158,6 +188,35 @@ watchEffect(() => {
   }
 });
 
+const validateName = (value) => {
+  if (!value) return "Name is required.";
+  return true;
+};
+
+const { value: name, errorMessage: nameError } = useField("name", validateName);
+
+watch(name, (newValue) => {
+  tour.value.name = newValue;
+});
+
+const validateAirline = (value) => {
+  if (!value) return "Airline is required.";
+  return true;
+};
+
+const { value: airline, errorMessage: airlineError } = useField(
+  "airline",
+  validateAirline
+);
+
+watch(airline, (newValue) => {
+  tour.value.airline = newValue;
+});
+
+const onSubmit = handleSubmit(() => {
+  console.log(tour.value);
+});
+
 onMounted(async () => {
   if (route.params.tourId) {
     const mountedData = data.tours.find((item) => {
@@ -176,11 +235,8 @@ onMounted(async () => {
     tour.value.image = URL.createObjectURL(fileBlob);
     tour.value.fileName = fileName;
 
-    console.log(tour.value);
+    name.value = mountedData.name;
+    airline.value = mountedData.airline;
   }
-});
-
-watch(tour.value, (newValue) => {
-  console.log(newValue);
 });
 </script>
