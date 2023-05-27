d14b4ddaf625fa4e597cb06f6f12d9bfda8b6e77
ppoonnamee <ppoonnamee@gmail.com>
2023-05-28 03:23:29 +0700

    navbar done

--- src/layout/Navbar.vue
+++ src/layout/Navbar.vue
@@ -40,25 +40,27 @@
   </nav> -->
 
   <nav class="navbar text-primary-blue relative z-10">
-    <div class="flex justify-between items-center py-4">
+    <div class="flex justify-between items-center py-4 px-3 md:px-0">
       <div class="flex items-center">
         <img src="@/assets/images/logo.png" alt="" />
-        <h4 class="ml-4 self-center font-semibold w-40 logo-text">
+        <h4 class="ml-4 self-center font-semibold w-40 text-primary-blue">
           Wellness Life Travel
         </h4>
       </div>
-      <div class="hidden md:flex md:justify-center md:items-center gap-28">
+      <div
+        class="hidden md:flex md:justify-center md:items-center sm:gap-8 lg:gap-16 xl:gap-28"
+      >
         <router-link
           v-for="(menuItem, index) in menuItems"
           :key="index"
           :to="menuItem.route"
-          class="px-4 py-2 mx-2 rounded hover:opacity-70"
+          class="py-0 mx-2 hover:opacity-70"
         >
           {{ menuItem.label }}
         </router-link>
       </div>
       <div
-        class="hidden md:flex items-center bg-primary-blue w-60 h-20 pl-4 pr-2 rounded-l-[5rem] justify-around"
+        class="hidden lg:flex items-center bg-primary-blue w-60 h-20 pl-4 pr-2 rounded-l-[5rem] justify-around"
       >
         <a
           v-for="(social, index) in socialLinks"
@@ -66,17 +68,33 @@
           :href="social.url"
           class="text-white hover:text-gray-400 ml-4"
         >
-          <font-awesome-icon
-            :icon="social.icon"
-            size="2xl"
-            style="color: #ffffff"
-            class="self-center"
-          />
+          <font-awesome-icon :icon="social.icon" size="2xl" />
         </a>
       </div>
       <div class="md:hidden">
-        <button @click="showMenu = !showMenu" class="focus:outline-none">
+        <!-- <button @click="showMenu = !showMenu" class="focus:outline-none">
           <font-awesome-icon :icon="['fas', 'bars']" size="2xl" />
+        </button> -->
+        <button
+          @click="showMenu = !showMenu"
+          class="flex items-center space-x-2 focus:outline-none"
+        >
+          <div class="w-9 flex items-center justify-center relative">
+            <span
+              :class="showMenu ? 'translate-y-0 rotate-45' : '-translate-y-3'"
+              class="hamburger-bar"
+            ></span>
+
+            <span
+              :class="showMenu ? 'opacity-0 translate-x-3' : 'opacity-100'"
+              class="hamburger-bar"
+            ></span>
+
+            <span
+              :class="showMenu ? 'translate-y-0 -rotate-45' : 'translate-y-3'"
+              class="hamburger-bar"
+            ></span>
+          </div>
         </button>
       </div>
     </div>
@@ -86,8 +104,12 @@
         class="md:hidden absolute inset-x-0 top-[129px] bg-[#333] z-20"
         style="height: calc(100vh - 50px)"
       >
-        <div class="flex flex-col px-4">
-          <div v-for="(item, index) in menuItems" :key="index" class="py-4">
+        <div class="px-6">
+          <div
+            v-for="(item, index) in menuItems"
+            :key="index"
+            class="py-4 border-b-[1px] border-white/20"
+          >
             <a class="!text-white hover:text-gray-300" :href="item.route">{{
               item.label
             }}</a>
@@ -101,6 +123,7 @@
 <script setup>
 import { ref } from "vue";
 /* eslint-disable */
+const open = ref(false);
 const showMenu = ref(false);
 const menuItems = ref([
   {
@@ -127,15 +150,6 @@ const socialLinks = ref([
 <style lang="scss" scoped>
 @import "@/assets/scss/variables.scss";
 
-a,
-.logo-text {
-  color: $primary-color;
-}
-
-.social-media-bg {
-  background-color: $secondary-color;
-}
-
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.3s ease;
@@ -147,6 +161,6 @@ a,
 }
 
 .router-link-active {
-  text-decoration: underline;
+  border-bottom: 2px solid $secondary-color;
 }
 </style>
