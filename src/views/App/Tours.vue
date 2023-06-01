96d263dae5e8462b9d1dd70a24dd5e70743d380a
Apikritch Rattanapisankul <apikritch.r@gmail.com>
2023-06-02 05:22:11 +1000

    added feature search, filter, sort, and pagination

--- src/views/App/Tours.vue
+++ src/views/App/Tours.vue
@@ -20,54 +20,130 @@
       </Button>
     </div>
 
-    <TourPackagesList />
+    <TourPackagesList :tours="paginatedTours" />
     <Paginator
-      :rows="10"
-      :totalRecords="120"
-      :rowsPerPageOptions="[10, 20, 30]"
+      :rows="pageSize"
+      :totalRecords="dataLength"
+      :rowsPerPageOptions="pageSizeOptions"
       class="pb-16"
+      @page="handlePageChange"
     ></Paginator>
   </div>
 </template>
 
 <script setup>
-import { ref, watch } from "vue";
+import { ref, watch, onMounted, computed } from "vue";
+import { collection, onSnapshot, query } from "firebase/firestore";
+import { db } from "@/firebase";
+
 import Toolbar from "@/components/Toolbar.vue";
 import TourPackagesList from "@/components/TourPackagesList.vue";
 
+const tours = ref([]);
+const search = ref("");
+const pageSizeOptions = ref([2, 4, 6]);
+const pageSize = ref(2);
+const dataLength = ref();
+const page = ref(0);
+
 const selectedCountry = ref();
 const countries = ref([
-  { name: "ออสเตรเลีย" },
-  { name: "บราซิล" },
-  { name: "จีน" },
-  { name: "เชค" },
-  { name: "สโลวัก" },
-  { name: "ฮังการี" },
+  { name: "ทั้งหมด", country: null },
+  { name: "ออสเตรเลีย", country: "ออสเตรเลีย" },
+  { name: "บราซิล", country: "บราซิล" },
+  { name: "จีน", country: "จีน" },
+  { name: "เชค", country: "เชค" },
+  { name: "สโลวัก", country: "สโลวัก" },
+  { name: "ฮังการี", country: "ฮังการี" },
 ]);
 
 const selectedSort = ref();
 const sort = ref([
-  { name: "ราคา ต่ำ-สูง" },
-  { name: "ราคา สูง-ต่ำ" },
-  { name: "ตัวอักษร (ก-ฮ)" },
-  { name: "ตัวอักษร (ฮ-ก)" },
+  { name: "ราคา ต่ำ-สูง", field: "price", order: "asc" },
+  { name: "ราคา สูง-ต่ำ", field: "price", order: "desc" },
+  { name: "ตัวอักษร (ก-ฮ)", field: "name", order: "asc" },
+  { name: "ตัวอักษร (ฮ-ก)", field: "name", order: "desc" },
 ]);
 
-const search = ref("");
-
-watch(selectedCountry, (newValue, oldValue) => {
-  console.log(oldValue);
-  console.log(newValue);
+watch(selectedCountry, (newValue) => {
+  if (newValue?.country === null) {
+    selectedCountry.value = null;
+  }
 });
 
-watch(selectedSort, (newValue, oldValue) => {
-  console.log(oldValue);
-  console.log(newValue);
+const handlePageChange = (e) => {
+  pageSize.value = e.rows;
+  page.value = e.page;
+};
+
+const filteredTours = computed(() => {
+  let filtered = tours.value;
+
+  if (search.value) {
+    filtered = filtered.filter((tour) =>
+      tour.name.toLowerCase().includes(search.value.toLowerCase())
+    );
+  }
+
+  if (selectedCountry.value) {
+    filtered = filtered.filter((tour) =>
+      tour.countries.includes(selectedCountry.value.name)
+    );
+  }
+
+  return filtered;
 });
 
-watch(search, (newValue, oldValue) => {
-  console.log(oldValue);
-  console.log(newValue);
+const sortedTours = computed(() => {
+  let sorted = filteredTours.value;
+
+  if (selectedSort.value) {
+    sorted = sorted.slice().sort((a, b) => {
+      const fieldA = a[selectedSort.value.field];
+      const fieldB = b[selectedSort.value.field];
+
+      if (fieldA < fieldB) {
+        return selectedSort.value.order === "asc" ? -1 : 1;
+      }
+      if (fieldA > fieldB) {
+        return selectedSort.value.order === "asc" ? 1 : -1;
+      }
+      return 0;
+    });
+  }
+
+  return sorted;
+});
+
+const paginatedTours = computed(() => {
+  const startIndex = page.value * pageSize.value;
+  const endIndex = startIndex + pageSize.value;
+  return sortedTours.value.slice(startIndex, endIndex);
+});
+
+const loadData = async () => {
+  const collectionRef = collection(db, "tours");
+
+  let q = query(collectionRef);
+
+  onSnapshot(q, (querySnapshot) => {
+    const tourData = [];
+
+    querySnapshot.forEach((doc) => {
+      const tour = {
+        id: doc.id,
+        ...doc.data(),
+      };
+      tourData.push(tour);
+    });
+
+    tours.value = tourData;
+    dataLength.value = tourData.length;
+  });
+};
+
+onMounted(() => {
+  loadData();
 });
 </script>
 
