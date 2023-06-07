f1c1021bef2fcaae1b0ed252386811694a25754c
Apikritch Rattanapisankul <apikritch.r@gmail.com>
2023-06-08 03:34:18 +1000

    fixed responsive for tours page

--- src/components/TourPackagesList.vue
+++ src/components/TourPackagesList.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="grid grid-cols-1 grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-16"
+    class="grid grid-cols-1 grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 sm:gap-8 md:gap-10"
   >
     <div v-for="item in props.tours" :key="item.id">
       <TourPackageCard :item="item" />
