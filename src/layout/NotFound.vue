b29abe8e307120e810c810d4433270f83f1b1d7e
ppoonnamee <ppoonnamee@gmail.com>
2023-06-13 21:52:46 +0700

    fix 403 to be 401

--- src/layout/NotFound.vue
+++ src/layout/NotFound.vue
@@ -1,25 +1,20 @@
 <template>
   <div class="bg-gray-100 min-h-[calc(100vh_-_13rem)] grid">
-    <div class="flex justify-center">
+    <div class="flex flex-col items-center justify-center py-12">
       <img
         :src="vector3d"
         class="self-center w-120" />
-      <div class="flex flex-col justify-center pl-6">
-        <h1 class="text-8xl font-bold text-primary-blue font-mono">404</h1>
-        <p class="text-gray-600 text-lg">Page not found</p>
-        <small class="text-gray-500">
-          The page you are looking for does not exist.
-        </small>
-        <div class="mt-6">
-          <Button
-            label="กลับสู่หน้าหลัก"
-            rounded
-            class="!bg-primary-blue !border-none"
-            @click="$router.push('/')" />
-        </div>
-      </div>
+      <h1 class="text-6xl font-bold mt-4">404</h1>
+      <p class="text-2xl font-semibold mt-4">Page not found</p>
+      <p class="mt-2 text-gray-600">
+        The page you are looking for does not exist.
+      </p>
+      <router-link
+        to="/"
+        class="mt-8 px-6 py-3 text-lg text-white bg-primary-blue hover:opacity-90 rounded-full"
+        >กลับสู่หน้าหลัก</router-link
+      >
     </div>
-
     <div class="flex items-end justify-end">
       <small class="text-gray-400 pr-1">
         <a href="https://www.vecteezy.com/free-png/3d-location"
@@ -31,15 +26,5 @@
 </template>
 
 <script setup>
-// import { onMounted } from "vue";
-// import { useRouter } from "vue-router";
 import vector3d from "@/assets/images/vecteezy_3d-online.png";
-
-// const router = useRouter();
-
-// onMounted(() => {
-//   setTimeout(() => {
-//     router.push("/");
-//   }, 3000);
-// });
 </script>
--- src/layout/Unauthorised.vue
+++ src/layout/Unauthorised.vue
@@ -1,23 +1,15 @@
 <template>
   <div
-    class="flex flex-col items-center justify-center bg-red-500 min-h-[calc(100vh_-_13rem)]">
-    <h1 class="text-6xl font-bold text-white">403</h1>
-    <p class="text-2xl font-semibold text-white mt-4">Forbidden</p>
-    <p class="text-white mt-2">You are not authorized to access this page.</p>
+    class="flex flex-col items-center justify-center bg-gray-100 min-h-[calc(100vh_-_13rem)]">
+    <h1 class="text-6xl font-bold">401</h1>
+    <p class="text-2xl font-semibold mt-4">Unauthorized</p>
+    <p class="mt-2 text-gray-600">
+      You are not authorized to access this page.
+    </p>
     <router-link
       to="/"
-      class="mt-8 px-6 py-3 text-lg font-semibold text-white bg-red-700 rounded hover:bg-red-800"
-      >Go back to Home</router-link
+      class="mt-8 px-6 py-3 text-lg text-white bg-primary-blue hover:opacity-90 rounded-full"
+      >กลับสู่หน้าหลัก</router-link
     >
   </div>
 </template>
-
-<script>
-export default {
-  name: "Forbidden",
-};
-</script>
-
-<style>
-/* Add additional styles or Tailwind CSS classes here */
-</style>
