fe1d1d17cacc3c8658232799ca00707543051c91
ppoonnamee <ppoonnamee@gmail.com>
2023-06-15 02:24:50 +0700

    set public path

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("@vue/cli-service");
 const path = require("path");
 
 module.exports = defineConfig({
+  publicPath: "/travel-agent/",
   transpileDependencies: true,
   configureWebpack: {
     resolve: {
