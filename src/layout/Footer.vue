11831ef7334593797ea2b4d06271f07dc1b45ec2
Apikritch Rattanapisankul <apikritch.r@gmail.com>
2023-06-15 06:08:37 +1000

    fixed 4

--- src/layout/Footer.vue
+++ src/layout/Footer.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="h-16 flex justify-center items-center bg-black text-white">
-    © Copyright 2023 All Rights Reserved.
+    <p>© Copyright 2023 All Rights Reserved.</p>
+    <router-link
+      to="/travel-agent/admin"
+      class="ml-8"
+      >ADMIN</router-link
+    >
   </div>
 </template>
