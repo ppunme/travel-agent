a54b26f038d69688588bbb12fb7f87b53a92b381
Apikritch Rattanapisankul <apikritch.r@gmail.com>
2023-06-19 01:16:42 +1000

    added event for google analytics

--- src/utils/VueGtag.js
+++ src/utils/VueGtag.js
@@ -1,6 +1,6 @@
-import VueGtag from "vue-gtag";
+import VueGtag, { event } from "vue-gtag";
 
-export const Gtag = [
+const Gtag = [
   [
     VueGtag,
     {
@@ -10,3 +10,47 @@ export const Gtag = [
     },
   ],
 ];
+
+const line = (page) => {
+  event("LineButtonClick", {
+    event_category: "Contact",
+    event_label: `Line button clicked through ${page} page`,
+  });
+};
+
+const messenger = (page) => {
+  event("MessengerButtonClick", {
+    event_category: "Contact",
+    event_label: `Messenger button clicked through ${page} page`,
+  });
+};
+
+const phone = (page) => {
+  event("PhoneButtonClick", {
+    event_category: "Contact",
+    event_label: `Phone button clicked through ${page} page`,
+  });
+};
+
+const email = (page) => {
+  event("EmailButtonClick", {
+    event_category: "Contact",
+    event_label: `Email button clicked through ${page} page`,
+  });
+};
+
+const facebook = (page) => {
+  event("FacebookButtonClick", {
+    event_category: "Social Media",
+    event_label: `Facebook button clicked through ${page} page`,
+  });
+};
+
+const instagram = (page) => {
+  event("InstagramButtonClick", {
+    event_category: "Social Media",
+    event_label: `Instagram button clicked through ${page} page`,
+  });
+};
+
+export { Gtag, line, messenger, phone, email, facebook, instagram };
